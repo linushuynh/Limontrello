f3f63878aa22d5393736a17ef653f01ae027851e
linushuynh <linusmhuynh@gmail.com>
2022-12-20 15:24:00 -0800

    seed dunder init file added truncate for production

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -12,11 +12,15 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
+        # Before seeding, truncate all tables prefixed with schema name
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        # Add a truncate command here for every table that will be seeded.
+        db.session.commit()
         # Before seeding in production, you want to run the seed undo
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
+        # undo_users()
     seed_users()
     # Add other seed functions here
 
