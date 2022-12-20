b05bcd772a7fe5ca573c405fa42d81c15a451707
linushuynh <linusmhuynh@gmail.com>
2022-12-20 15:16:11 -0800

    migrations added

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
 from .db import db
 from .user import User
+from .board import Board
+from .card import Card
+from .list import CardList
+from .comment import Comment
 from .db import environment, SCHEMA
