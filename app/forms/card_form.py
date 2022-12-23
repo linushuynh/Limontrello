e6ca6dc498125121ec02acdeca3d631a95d31a22
linushuynh <linusmhuynh@gmail.com>
2022-12-23 10:43:16 -0800

    get cards from list, create card, update card, delete card routes added

---
+++ app/forms/card_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired, ValidationError
+
+
+class CardForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    description = StringField('Description')
+    list_id = IntegerField('list_id', validators=[DataRequired()])
