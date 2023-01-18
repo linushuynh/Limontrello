069f31db34fc353ac5e53b9069604774f3bb1cc5
linushuynh <linusmhuynh@gmail.com>
2023-01-17 23:07:01 -0800

    list routes

--- app/api/list_routes.py
+++ app/api/list_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import Card, db, CardList
-# from ..forms.board_form import CreateBoardForm, UpdateBoardForm
+from ..forms import ListForm
 from .auth_routes import validation_errors_to_error_messages, authorized
 
 list_routes = Blueprint('lists', __name__)
@@ -25,3 +25,65 @@ def get_cards(list_id):
         return { 'errors': 'There were no cards in this list' }
 
     return { 'cards': cards }
+
+
+@list_routes.route('', methods=["POST"])
+@login_required
+def create_list():
+    """
+    Creates a new list using form data
+    """
+    form = ListForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    if form.validate_on_submit():
+        data = form.data
+        new_list = CardList(
+            name = data['name'],
+            board_id = data['board_id']
+        )
+        db.session.add(new_list)
+        db.session.commit()
+        return new_list.to_dict()
+
+    return { 'errors' : validation_errors_to_error_messages(form.errors)}, 401
+
+
+@list_routes.route('/<int:list_id>', methods=["PUT"])
+@login_required
+def update_list(list_id):
+    """
+    Update list with form data at the given list id
+    """
+    form = ListForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    selected_list = CardList.query.get(list_id)
+
+    if not selected_list:
+        return { "error": "List couldn't be found" }, 404
+
+    if form.validate_on_submit():
+        data = form.data
+        selected_list.name = data['name']
+        selected_list.board_id = data['board_id']
+
+        db.session.commit()
+        return selected_list.to_dict()
+
+    return { "errors": validation_errors_to_error_messages(form.errors) }, 401
+
+
+@list_routes.route('/<int:list_id>/delete', methods=['DELETE'])
+@login_required
+def delete_list(list_id):
+    """
+    Queries for a list then deletes it
+    """
+    selected_list = CardList.query.get(list_id)
+
+    if not selected_list:
+        return { "error": "List couldn't be found" }, 404
+
+    db.session.delete(selected_list)
+    db.session.commit()
---
+++ app/forms/list_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, ValidationError, Length
+
+
+class ListForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(max=20, message="List name cannot be longer than 20 characters.")])
+    board_id = IntegerField('board_id', validators=[DataRequired()])
